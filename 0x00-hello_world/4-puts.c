28e4a07b456eba8086207bbeb20da67aff51b5f1
amankifle610 <amankifle610@gmail.com>
2022-12-07 21:04:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
- * main fun print a line
- * return 0
- */
-int main(){
+ * main - print a line of code
+ *
+ * return: 0
+*/
+int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
