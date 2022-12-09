784788f4f3ad0b89ff3784b1284bb069341212df
amankifle610 <amankifle610@gmail.com>
2022-12-09 07:10:56 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		putchar(a + '0');
+		putchar(',');
+		putchar(' ');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
