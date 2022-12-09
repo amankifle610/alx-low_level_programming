1e7c82c5b66ba338146a9df582f02fae7c0a0ad1
amankifle610 <amankifle610@gmail.com>
2022-12-09 07:30:55 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int a, b, c;
+
+	a=0;
+	b=1;
+	while (a < 10)
+	{
+		c = b;
+		while (c <= 10)
+		{
+			if(c < 10)
+			{
+			putchar(a + '0');
+			putchar(c + '0');
+			}
+			c++;
+			putchar(',');
+			putchar(' ');
+		}
+		b++;
+		a++;
+	}
+	return (0);
+}
