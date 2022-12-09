48915eff1e26c37d5cc7e497b6b8f44b1c06dc58
amankifle610 <amankifle610@gmail.com>
2022-12-09 08:29:18 -0800

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,4 +6,34 @@
 */
 int main(void)
 {
+	int a, b, c, d;
+
+	a = 0;
+	b = 1;
+	while (a < 8)
+	{
+		d = b;
+		while (b < 9)
+		{
+			c = b + 1;
+			while (c < 10)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				c++;
+				if(a != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		b = d + 1;
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
 
