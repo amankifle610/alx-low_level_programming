8b06624726d279b5a9df6d7e431549a8d7385fc1
amankifle610 <amankifle610@gmail.com>
2022-12-13 08:44:04 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -15,4 +15,5 @@ void print_alphabet(void)
 		_putchar(n);
 		n++;
 	}
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return
+*/
+void print_alphabet_x10(void)
+{
+	char a;
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		a = 'a';
+		for (j = 0; j < 26 ; j++)
+		{
+			_putchar(a);
+			a++;
+		}
+	}
+}
