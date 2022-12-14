2b5636c394b3bf037dfc5f56982af2ac19c3ee60
amankifle610 <amankifle610@gmail.com>
2022-12-13 23:14:43 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table
+ * @n : any number
+ * Return:
+*/
+void print_times_table(int n)
+{
+	int i, j, x, y;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			x = j * i;
+			y = x;
+			if (x == 0)
+			{
+				_putchar(x + '0');
+				if (i == n)
+					_putchar('\n');
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			while (x > 0)
+			{
+				y = x % 10;
+				x = x / 10;
+				_putchar(y + '0');
+				if (i == 0)
+					_putchar('\n');
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,13 +3,13 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n : any number
- * Return: 
+ * Return: 0
 */
 void print_to_98(int n)
 {
 	if (n == 98)
 		printf("%d\n", n);
-	else if ( n > 98)
+	else if (n > 98)
 	{
 		while (n > 98)
 		{
