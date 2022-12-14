cff0354b861e7461a5c35d7636a19f2f4ca8ca15
amankifle610 <amankifle610@gmail.com>
2022-12-14 07:07:26 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,8 @@
 */
 void print_times_table(int n)
 {
-       	int i, j, x, y;
+	int i, j, x, y;
+
 	if (n > 15 || n < 0)
 		return;
 	for (i = 0; i <= n; i++)
@@ -21,10 +22,9 @@ void print_times_table(int n)
 				_putchar((y / 10) + '0');
 			}
 			else if (x > 9)
-			{
-				_putchar((x / 10) + '0');
 				if (j > 0)
 				_putchar(' ');
+				_putchar((x / 10) + '0');
 			}
 			else if (j > 0)
 			{
@@ -39,5 +39,5 @@ void print_times_table(int n)
 			}
 		}
 		_putchar('\n');
-	}  
+}
 }
