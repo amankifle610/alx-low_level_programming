f297cc478a433f3b53258f9c125ebe7bc6d0f303
amankifle610 <amankifle610@gmail.com>
2022-12-14 07:38:09 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int x, y, i, f; 
+
+	x = 1;
+	y = 2;
+	printf("%d, ", 1);
+	printf("%d, ", 2);
+	for (i = 0; i < 48; i++)
+	{
+		f = x + y;
+		x = y;
+		y = f;
+		printf("%d", f);
+		if (i < 47)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
