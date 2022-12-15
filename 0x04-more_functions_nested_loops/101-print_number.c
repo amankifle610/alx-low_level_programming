db1bea2c02dff4167563505beb8fb09b251c5d1a
amankifle610 <amankifle610@gmail.com>
2022-12-15 09:01:21 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,12 +9,6 @@ void print_number(int n)
 	int x, i;
 
 	i = 1000000000;
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
 		if (n < 0)
 		{
 			_putchar('-');
@@ -25,7 +19,6 @@ void print_number(int n)
 		for (; i >= 1; i /= 10)
 		{
 			if (x / i != 0)
-				_putchar((m / i) % 10 + '0');
+				_putchar((x / i) % 10 + '0');
 		}
-	}
 }
