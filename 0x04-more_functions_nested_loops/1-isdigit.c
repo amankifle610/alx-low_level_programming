6450763aafc986468b9c1ef6efb4c43cd86a3922
amankifle610 <amankifle610@gmail.com>
2022-12-15 05:54:26 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * _isdigit - digit
@@ -6,7 +7,7 @@
 */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	       return (1);
+	if (isdigit(c))
+		return (1);
 	return (0);
 }
