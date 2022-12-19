2bcbbf800751d9a48c8db13076f1d05223ec8b0e
amankifle610 <amankifle610@gmail.com>
2022-12-19 05:21:31 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * print_array - array
- * @a - array
- * @n - num
+ * @a : array
+ * @n : num
  * Return:
 */
 void print_array(int *a, int n)
@@ -11,11 +11,21 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
+		if(a[i] < 0)
+		{
+			_putchar('-');
+			a[i] = -a[i];
+		}
 		h = 100000;
 		for (; h >= 1; h /= 10)
 		{
 			if (a[i] / h != 0)
 				_putchar((a[i] / h) % 10 + '0');
+		}
+		if (i < n -1)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
