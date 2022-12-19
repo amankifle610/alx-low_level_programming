6e92935da8fdcd4d9f5fdd3a32dc73b4850a1c4d
amankifle610 <amankifle610@gmail.com>
2022-12-19 05:24:30 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,18 +11,18 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if(a[i] < 0)
+		if (a[i] < 0)
 		{
 			_putchar('-');
 			a[i] = -a[i];
 		}
-		h = 100000;
+		h = 10000000;
 		for (; h >= 1; h /= 10)
 		{
 			if (a[i] / h != 0)
 				_putchar((a[i] / h) % 10 + '0');
 		}
-		if (i < n -1)
+		if (i < n - 1)
 		{
 			_putchar(',');
 			_putchar(' ');
