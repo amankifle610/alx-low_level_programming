d700d33c3f86dee7c8b0636cfa5fbba65e8d5c26
amankifle610 <amankifle610@gmail.com>
2022-12-22 09:20:36 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - to upper case
+ * @a : string
+ * Return: string
+*/
+char *string_toupper(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i])
+	{
+		if (a[i] >= 97)
+			a[i] + = 32;
+		i++;
+	}
+	return (a);
+}
