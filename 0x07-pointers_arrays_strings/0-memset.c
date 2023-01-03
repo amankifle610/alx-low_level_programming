305b18be8923a42665167b092014b182cbba23ea
amankifle610 <amankifle610@gmail.com>
2023-01-03 05:42:04 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills memory
+ * @s : memory
+ * @b : byte
+ * @n : byte filled
+ * Return: pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		*(s + a) = b;
+	}
+	return (b);
+}
