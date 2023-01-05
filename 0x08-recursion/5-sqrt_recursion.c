a9853ba1538def22d7c374967bf37aefde0311d8
amankifle610 <amankifle610@gmail.com>
2023-01-05 04:58:47 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,19 @@
 #include "main.h"
+/**
+ * _sqrt_recursion - square root
+ * @n : number
+ * Return: square root
+*/
+int _sqrt_recursion(int n)
+{
+	static int x = 0;
+
+	if (n < 0)
+		return (-1);
+	if (x * x == n)
+		return (x);
+	else if (x == n)
+		return (-1);
+	x++;
+	_sqrt_recursion(n);
+}
