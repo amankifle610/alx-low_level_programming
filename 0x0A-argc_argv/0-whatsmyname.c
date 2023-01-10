1b370b718ea2c00b4b44ddf38fd5721dc9e5e723
amankifle610 <amankifle610@gmail.com>
2023-01-10 03:31:35 -0800

    4-add.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argc: number
+ * @argv: command line arguments
+ * Return: 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
